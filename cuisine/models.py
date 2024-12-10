d9361a8db9b5b85ecfa7a63a62c3e57000a16bcd
theKason <jinjiehe@outlook.com.au>
2024-12-10 22:04:12 +1100

    models changed

--- cuisine/models.py
+++ cuisine/models.py
@@ -1,17 +1,14 @@
 from django.db import models
 
 # Create your models here.
-class Category(models.Model):
-    # 菜的类别
-    name = models.CharField(max_length=5)
-
 class Cuisine(models.Model):
     # 菜的名称
     name = models.CharField(max_length=20)
     # 菜的价格
     price = models.IntegerField()
     # 菜的描述
-    desc = models.TextField()
+    desc = models.TextField(blank=True)
     # 菜的图片
-    # 菜的归类
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    # 菜的类别：这里使用 enumeration classes 来定义菜的类别
+    cuisineType = models.TextChoices("Meat","Vegetable","Dessert","Beverage")
+        
