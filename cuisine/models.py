7445ee2a2e43156415846fd4e5f34b4482207cef
theKason <jinjiehe@outlook.com.au>
2024-12-10 13:33:56 +1100

    创建cuisine应用的models

--- cuisine/models.py
+++ cuisine/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    # 菜的类别
+    name = models.CharField(max_length=5)
+
+class Cuisine(models.Model):
+    # 菜的名称
+    name = models.CharField(max_length=20)
+    # 菜的价格
+    price = models.IntegerField()
+    # 菜的描述
+    desc = models.TextField()
+    # 菜的图片
+    # 菜的归类
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
