b39a5316a4981e34a96fa802b11e188d6ad23ced
theKason <jinjiehe@outlook.com.au>
2024-12-11 16:50:17 +1100

    models,views completed

--- menu_system/settings.py
+++ menu_system/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'menu_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': BASE_DIR / 'menu_system_db',
+        'HOST': '127.0.0.1',
+        'USER': 'menu_system',
+        'PASSWORD': '123456',
     }
 }
 
